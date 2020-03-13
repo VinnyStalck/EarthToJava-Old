e9796958c87d7b62d82180cfd9754bc360803196
StalckGx <vinny.polsom@outlook.com>
2020-03-13 12:49:06 -0300

    Horn Item added

--- src/main/java/baunilla/earthtojava/init/ItemInit.java
+++ src/main/java/baunilla/earthtojava/init/ItemInit.java
@@ -12,4 +12,5 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, EarthToJava.MOD_ID);
 
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> HORN = ITEMS.register("horn", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
 }
