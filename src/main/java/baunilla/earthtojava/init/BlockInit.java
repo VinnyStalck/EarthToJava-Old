dd49b5c51333d098a4bc144f974acf3e813f09cf
StalckGx <vinny.polsom@outlook.com>
2020-03-17 13:13:31 -0300

    HarvestLevel and HarvestTool for Ruby block added

--- src/main/java/baunilla/earthtojava/init/BlockInit.java
+++ src/main/java/baunilla/earthtojava/init/BlockInit.java
@@ -8,6 +8,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.potion.Effects;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,8 +16,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, EarthToJava.MOD_ID);
 
-    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", () -> new Block(Block.Properties.create(Material.IRON, MaterialColor.RED).hardnessAndResistance(5.0f, 6.0f).sound(SoundType.METAL)));
-    public static final RegistryObject<Block> RUBY_ORE = BLOCKS.register("ruby_ore", () -> new OreBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f)));
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", () -> new Block(Block.Properties.create(Material.IRON, MaterialColor.RED).hardnessAndResistance(5.0f, 6.0f).sound(SoundType.METAL).harvestLevel(2).harvestTool(ToolType.PICKAXE)));
+    public static final RegistryObject<Block> RUBY_ORE = BLOCKS.register("ruby_ore", () -> new OreBlock(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0f).harvestLevel(2)));
     
     public static final RegistryObject<Block> BUTTERCUP = BLOCKS.register("buttercup", () -> new FlowerBlock(Effects.SATURATION, 7, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().hardnessAndResistance(0.0f).sound(SoundType.PLANT)));
 }
