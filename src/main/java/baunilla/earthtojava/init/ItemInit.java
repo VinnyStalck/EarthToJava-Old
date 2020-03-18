a05e8f61253dd1b71ef5b8fac833a9baf6186f12
StalckGx <vinny.polsom@outlook.com>
2020-03-18 12:01:31 -0300

    SpawnEgg item register

--- src/main/java/baunilla/earthtojava/init/ItemInit.java
+++ src/main/java/baunilla/earthtojava/init/ItemInit.java
@@ -1,9 +1,11 @@
 package baunilla.earthtojava.init;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Rarity;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,11 +15,18 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, EarthToJava.MOD_ID);
 
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS).rarity(Rarity.UNCOMMON)));
-    public static final RegistryObject<Item> HORN = ITEMS.register("horn", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+    public static final RegistryObject<Item> HORN = ITEMS.register("horn", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS))); 
 
-    // BlockItems
+    // Block Items
     public static final RegistryObject<Item> RUBY_BLOCK = ITEMS.register("ruby_block", () -> new BlockItem(BlockInit.RUBY_BLOCK.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS).rarity(Rarity.UNCOMMON)));
     public static final RegistryObject<Item> RUBY_ORE = ITEMS.register("ruby_ore", () -> new BlockItem(BlockInit.RUBY_ORE.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS).rarity(Rarity.UNCOMMON)));
 
     public static final RegistryObject<Item> BUTTERCUP = ITEMS.register("buttercup", () -> new BlockItem(BlockInit.BUTTERCUP.get(), new Item.Properties().group(ItemGroup.DECORATIONS)));
+
+    // Spawn Egg Items
+    public static Item registerEntitySpawnEgg(EntityType<?> type, int color1, int color2) {
+        SpawnEggItem EGG = new SpawnEggItem(type, color1, color2, new Item.Properties().group(ItemGroup.MISC));
+        return EGG;
+    }
+
 }
